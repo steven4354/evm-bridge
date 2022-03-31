541d26d82740390d5b6cccf8fcefba4499a03864
jack-shi0125 <jack.shi0125@gmail.com>
2022-04-01 07:51:55 +0800

    Implements postgres using heroku

--- tasks/db.ts
+++ tasks/db.ts
@@ -1,57 +1,63 @@
-// Importing SQLite3 to our project.
-var sqlite3 = require("sqlite3").verbose();
+// Importing Postgres to our project.
+const { Client } = require("pg");
+
+const credentials = {
+	user: "jgvgiyrqwmlrde",
+	host: "ec2-54-160-109-68.compute-1.amazonaws.com",
+	database: "d3outivvmf318g",
+	password: "77571d87f87c9692c402fcdddab8d175059b65dda47baf50ab2b5f4f9aa12674",
+	port: 5432,
+	ssl: {
+		rejectUnauthorized: false
+	}
+};
+
+// const table = `
+// 	CREATE TABLE records (
+// 		id TEXT,
+// 		fromchain TEXT,
+// 		tochain TEXT,
+// 		fromaddr TEXT,
+// 		toaddr TEXT,
+// 		amount TEXT,
+// 		state TEXT
+// 	);
+// `
 
 export async function addRecord(
 	id: string,
 	fromchain: string, tochain: string,
 	from: string, to: string, amount: number) {
-    const db = new sqlite3.Database("db.db");
-	return new Promise((resolve, reject) => {
-		db.run("INSERT INTO records VALUES ($id, $fromchain, $tochain, $from, $to, $amount, $state)", {
-			$id: id,
-			$fromchain: fromchain,
-			$tochain: tochain,
-			$from: from,
-			$to: to,
-			$amount: amount,
-			$state: 'LOCKED'
-		}, function (err: any) {
-			if (err) reject(err);
-			else {
-                db.close();
-                resolve(true);
-            }
-		});
-	});
+	const text = `
+		INSERT INTO records (id, fromchain, tochain, fromaddr, toaddr, amount, state)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		RETURNING id
+	`;
+	const values = [id, fromchain, tochain, from, to, amount.toString(), 'LOCKED'];
+	const client = new Client(credentials);
+	await client.connect();
+	const addedRecord = await client.query(text, values);
+	await client.end();
+  	return addedRecord.rows;
 }
 
 
-export function updateState(id: string, state: string) {
-    const db = new sqlite3.Database("db.db");
-    var inputData = [state, id];
-	return new Promise((resolve, reject) => {
-        db.run("UPDATE records SET state=? WHERE id=?", inputData, function (err: any, rows: any) {
-            if (err) reject(err);
-            else {
-                console.log(inputData, "ok");
-                db.close();
-                resolve(rows);
-            }
-        });
-	});
+export async function updateState(id: string, state: string) {
+    const text = `UPDATE records SET state=$1 WHERE id=$2`;
+    var values = [state, id];
+	const client = new Client(credentials);
+	await client.connect();
+	const updatedRecord = await client.query(text, values);
+	await client.end();
+  	return updatedRecord.rows;
 }
 
-export function dumpRecords() {
+export async function dumpRecords() {
 	// Add a task to the todo list.
-    const db = new sqlite3.Database("db.db");
-	return new Promise((resolve, reject) => {
-		db.all("SELECT * FROM records",
-			function (err: any, rows: any) {
-				if (err) reject(err);
-				else {
-                    db.close();
-                    resolve(rows);
-                }
-			});
-	});
+	const text = `SELECT * FROM records`;
+	const client = new Client(credentials);
+	await client.connect();
+	const records = await client.query(text);
+	await client.end();
+  	return records.rows;
 }
